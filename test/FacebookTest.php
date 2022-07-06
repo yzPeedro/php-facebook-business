c2cb8fca3c777e5dfeaabb5248d5d90cb239d3e0
yzPeedro <pedrocruzpessoa16@gmail.com>
2022-07-06 08:17:42 -0300

    add: facebook test

---
+++ test/FacebookTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Meta\FacebookSDK\Facebook;
+
+class FacebookTest extends TestCase
+{
+    public function testJavascriptHelper()
+    {
+        $facebook = new Facebook();
+        $javascript = $facebook->helper->javascriptSDK('pt_BR', false);
+
+        $this->assertIsString($javascript);
+    }
+
+    public function testJavascriptLogin()
+    {
+        $facebook = new Facebook();
+        $javascript = $facebook->helper->javascriptLogin();
+
+        $this->assertIsString($javascript);
+    }
+
+    public function testJavascriptLogout()
+    {
+        $facebook = new Facebook();
+        $javascript = $facebook->helper->javascriptLogout();
+
+        $this->assertIsString($javascript);
+    }
+}
