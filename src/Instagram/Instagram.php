037e828d75887aae1c6cc9df399186e651e8b70f
yzPeedro <pedrocruzpessoa16@gmail.com>
2022-07-11 10:54:47 -0300

    update: instagram basic display api methods

--- src/Instagram/Instagram.php
+++ src/Instagram/Instagram.php
@@ -3,10 +3,23 @@
 namespace Meta\InstagramSDK;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\GuzzleException;
+use Meta\InstagramSDK\Exception\InstagramException;
 
 class Instagram
 {
+    private const IG_BASE_URI = "https://api.instagram.com";
+
+    /**
+     * @var InstagramHelper
+     */
     public InstagramHelper $helper;
+
+    /**
+     * @var string
+     */
+    private string $access_token;
 
     public function __construct(
         private string|int $app_id,
@@ -38,5 +51,72 @@ class Instagram
     public function getRedirectUri(): string
     {
         return $this->redirect_uri;
+    }
+
+    /**
+     * @param string $access_token
+     * @return $this
+     */
+    public function setAccessToken(string $access_token): static
+    {
+        $this->access_token = $access_token;
+        return $this;
+    }
+
+    /**
+     * @param string|int $user_id
+     * @param array|string[] $fields
+     * @return mixed
+     * @throws GuzzleException
+     * @throws InstagramException
+     */
+    public function getUser(string|int $user_id, array $fields = ['account_type', 'id', 'media_count', 'username']): mixed
+    {
+        $data = [ 'access_token' => $this->getAccessToken(), 'fields' => implode(',', $fields) ];
+
+        try {
+            $client = new Client(['base_uri' => self::IG_BASE_URI]);
+            $response = $client->request('GET', "/$user_id?".http_build_query($data));
+
+            return json_decode($response->getBody());
+        } catch (ClientException $exception) {
+            throw new InstagramException($exception->getResponse()->getBody(), $exception->getCode());
+        }
+    }
+
+    /**
+     * @param string $user_id
+     * @param array $options
+     * @return mixed
+     * @throws GuzzleException
+     * @throws InstagramException
+     */
+    public function getMedia(string $user_id, array $options = []): mixed
+    {
+        $data = [ 'access_token' => $this->getAccessToken() ];
+
+        foreach ($options as $index => $option) {
+            if (is_array($option)) {
+                $options[$index] = implode(',', $option);
+            }
+            $data[$index] = $options[$index];
+        }
+
+        try {
+            $client = new Client(['base_uri' => self::IG_BASE_URI]);
+            $response = $client->request('GET', "/$user_id/media?".http_build_query($data));
+
+            return json_decode($response->getBody());
+        } catch (ClientException $exception) {
+            throw new InstagramException($exception->getResponse()->getBody(), $exception->getCode());
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccessToken(): string
+    {
+        return $this->access_token;
     }
 }
--- src/Instagram/InstagramHelper.php
+++ src/Instagram/InstagramHelper.php
@@ -2,10 +2,22 @@
 
 namespace Meta\InstagramSDK;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\GuzzleException;
+use Meta\InstagramSDK\Exception\InstagramException;
+
 class InstagramHelper
 {
     private const IG_BASE_URI = "https://api.instagram.com";
 
+    private const IG_GRAPH_URI = "https://graph.instagram.com";
+
+    /**
+     * @param string|int $app_id
+     * @param string|int $app_secret
+     * @param string $redirect_uri
+     */
     public function __construct(private string|int $app_id, private string|int $app_secret, private string $redirect_uri){}
 
     /**
@@ -32,6 +44,10 @@ class InstagramHelper
         return $this->redirect_uri;
     }
 
+    /**
+     * @param array|string[] $scope
+     * @return string
+     */
     public function getLink(array $scope = ['user_profile', 'user_media']): string
     {
         $params = [
@@ -42,5 +58,78 @@ class InstagramHelper
         ];
 
         return self::IG_BASE_URI."/oauth/authorize/?" . http_build_query($params);
+    }
+
+    /**
+     * @param string $code
+     * @return mixed
+     * @throws InstagramException
+     * @throws GuzzleException
+     */
+    public function getAccessToken(string $code): mixed
+    {
+        try {
+            $data = [
+                'client_id' => $this->getAppId(),
+                'client_secret' => $this->getAppSecret(),
+                'grant_type' => 'authorization_code',
+                'redirect_uri' => $this->getRedirectUri(),
+                'code' => $code
+            ];
+
+            $client = new Client(['base_uri' => self::IG_BASE_URI]);
+            $response = $client->request('GET', '/oauth/access_token?'.http_build_query($data));
+
+            return json_decode($response->getBody());
+        } catch (ClientException $exception) {
+            throw new InstagramException($exception->getResponse()->getBody(), $exception->getCode());
+        }
+    }
+
+    /**
+     * @param string $token
+     * @return mixed
+     * @throws GuzzleException
+     * @throws InstagramException
+     */
+    public function getLoginLivedToken(string $token): mixed
+    {
+        try {
+            $data = [
+                'client_secret' => $this->getAppSecret(),
+                'grant_type' => 'ig_exchange_token',
+                'access_token' => $token
+            ];
+
+            $client = new Client(['base_uri' => self::IG_GRAPH_URI]);
+            $response = $client->request('GET', '/access_token?'.http_build_query($data));
+
+            return json_decode($response->getBody());
+        } catch (ClientException $exception) {
+            throw new InstagramException($exception->getResponse()->getBody(), $exception->getCode());
+        }
+    }
+
+    /**
+     * @param string $token
+     * @return mixed
+     * @throws GuzzleException
+     * @throws InstagramException
+     */
+    public function refreshAccessToken(string $token): mixed
+    {
+        try {
+            $data = [
+                'grant_type' => 'ig_refresh_token',
+                'access_token' => $token
+            ];
+
+            $client = new Client(['base_uri' => self::IG_GRAPH_URI]);
+            $response = $client->request('GET', '/refresh_access_token?'.http_build_query($data));
+
+            return json_decode($response->getBody());
+        } catch (ClientException $exception) {
+            throw new InstagramException($exception->getResponse()->getBody(), $exception->getCode());
+        }
     }
 }
